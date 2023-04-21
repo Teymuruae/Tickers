ff1d0c701e17205ebb209608b903ff00de3b84ec
Teymur Baratov <baratov.teymur@yandex.ru>
2023-04-21 19:10:48 +0300

    readme

---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,45 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ApiTest {
+
+    private String url = "https://api.kucoin.com/api/v1/market/allTickers";
+
+    public List<Tickers> callApi() {
+        return RestAssured
+                .given()
+                .when()
+                .get(url)
+                .then()
+                .extract().jsonPath().getList("data.ticker", Tickers.class);
+
+    }
+
+    @Test
+    public void test() {
+        List<Tickers> tickers = callApi();
+        List<Tickers> usdtTickers = tickers.stream().filter(x -> x.getSymbol().endsWith("USDT"))
+                .collect(Collectors.toList());
+        Assertions.assertTrue(usdtTickers.stream().allMatch(x -> x.getSymbol().endsWith("USDT")));
+    }
+
+    @Test
+    public void sortedTest(){
+        List<Tickers> sortedTickers = callApi().stream().filter(x->x.getSymbol().endsWith("USDT"))
+                .sorted(new Comparator<Tickers>() {
+            @Override
+            public int compare(Tickers o1, Tickers o2) {
+                return o2.getChangeRate().compareTo( o1.getChangeRate());   //сортируем по changeRate от большего к меньшему
+
+            }
+                }).collect(Collectors.toList());
+        List<Tickers> top10 = sortedTickers.stream().limit(10).collect(Collectors.toList());
+
+        Assertions.assertEquals("OM-USDT", top10.get(0).getSymbol());
+    }
+}
---
+++ src/test/java/Tickers/Tickers.java
@@ -0,0 +1,4 @@
+package Tickers;
+
+public class Tickers {
+}
